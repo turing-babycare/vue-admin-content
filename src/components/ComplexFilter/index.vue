872f3e818b76e2cb7758732fda9cf3101edf45b4
汪鑫 <1598707976@qq.com>
2021-07-31 18:25:41 +0800

    fix: takeshow默认值

--- src/components/ComplexFilter/index.vue
+++ src/components/ComplexFilter/index.vue
@@ -1,256 +1,258 @@
 <template>
-  <div class="layout">
-    <a-table
-      style="background: #fff"
-      :dataSource="queryArray"
-      :columns="columns"
-      bordered
-      :pagination="false"
-    >
-      <template #type="text">
-        <div>
-          {{ originalFilterData.find((i) => i.value === text.key).label }}
-        </div>
-      </template>
-      <template #value="text">
-        <div v-if="text.type === 'tag'">
+  <div id="content">
+    <div class="layout">
+      <a-table
+        style="background: #fff"
+        :dataSource="queryArray"
+        :columns="columns"
+        bordered
+        :pagination="false"
+      >
+        <template #type="text">
           <div>
-            包含：{{
-              text.value && text.value.tag_contain
-                ? text.value.tag_contain.map((i) => i.label).join(',')
-                : '无'
-            }}
+            {{ originalFilterData.find((i) => i.value === text.key).label }}
           </div>
+        </template>
+        <template #value="text">
+          <div v-if="text.type === 'tag'">
+            <div>
+              包含：{{
+                text.value && text.value.tag_contain
+                  ? text.value.tag_contain.map((i) => i.label).join(',')
+                  : '无'
+              }}
+            </div>
+            <div>
+              不包含：{{
+                text.value && text.value.tag_no_contain
+                  ? text.value.tag_no_contain.map((i) => i.label).join(',')
+                  : '无'
+              }}
+            </div>
+          </div>
+          <div v-else-if="text.type === 'dateRange'">
+            {{ formatTime(text.value[0], '{y}-{m}-{d}') }}至{{
+              formatTime(text.value[1], '{y}-{m}-{d}')
+            }}
+            <div v-if="text.key === 'member_date'">
+              {{ getAgeOfMonthStr(text.value[1]) }} ~
+              {{ getAgeOfMonthStr(text.value[0]) }}
+            </div>
+          </div>
+          <div v-else-if="text.type === 'age'">
+            <span>{{ age_birthday_start }}至{{ age_birthday_end }}</span>
+            (<span
+              v-for="(item, index) in Object.keys(text.value.age_start)"
+              :key="index + 'satart'"
+            >
+              {{ text.value.age_start[item] }}
+              {{ item | ageType }}
+            </span>
+            <span>~</span>
+            <span
+              v-for="(item, index) in Object.keys(text.value.age_end)"
+              :key="index + 'end'"
+            >
+              {{ text.value.age_end[item] }}
+              {{ item | ageType }} </span
+            >)
+          </div>
+          <div v-else-if="text.type === 'input'">
+            {{ text.value }}
+          </div>
+          <div v-else>
+            {{ text.value.map((i) => i.lable || i.label).join(',') }}
+          </div>
+        </template>
+        <template #action="text, record, index">
           <div>
-            不包含：{{
-              text.value && text.value.tag_no_contain
-                ? text.value.tag_no_contain.map((i) => i.label).join(',')
-                : '无'
-            }}
+            <a-button type="link" @click="onFilterItemEdit(record, index)">
+              编辑
+            </a-button>
+            <a-button
+              type="link"
+              style="color: red"
+              @click="onFilterItemDelete(index)"
+            >
+              删除
+            </a-button>
           </div>
-        </div>
-        <div v-else-if="text.type === 'dateRange'">
-          {{ formatTime(text.value[0], '{y}-{m}-{d}') }}至{{
-            formatTime(text.value[1], '{y}-{m}-{d}')
+        </template>
+      </a-table>
+      <div class="footer">
+        <div :class="queryArray.length ? 'show' : 'hide'" v-if="taskShow">
+          当前筛选条件下共包含
+          <span class="number">{{
+            typeof shwoTotal !== 'undefined' ? shwoTotal : total
+          }}</span>
+          {{
+            originalFilterData.find((i) => i.label === '群主')
+              ? '个客户群'
+              : '名用户'
           }}
-          <div v-if="text.key === 'member_date'">
-            {{ getAgeOfMonthStr(text.value[1]) }} ~
-            {{ getAgeOfMonthStr(text.value[0]) }}
-          </div>
         </div>
-        <div v-else-if="text.type === 'age'">
-          <span>{{ age_birthday_start }}至{{ age_birthday_end }}</span>
-          (<span
-            v-for="(item, index) in Object.keys(text.value.age_start)"
-            :key="index + 'satart'"
-          >
-            {{ text.value.age_start[item] }}
-            {{ item | ageType }}
-          </span>
-          <span>~</span>
-          <span
-            v-for="(item, index) in Object.keys(text.value.age_end)"
-            :key="index + 'end'"
-          >
-            {{ text.value.age_end[item] }}
-            {{ item | ageType }} </span
-          >)
-        </div>
-        <div v-else-if="text.type === 'input'">
-          {{ text.value }}
-        </div>
-        <div v-else>
-          {{ text.value.map((i) => i.lable || i.label).join(',') }}
-        </div>
-      </template>
-      <template #action="text, record, index">
-        <div>
-          <a-button type="link" @click="onFilterItemEdit(record, index)">
-            编辑
-          </a-button>
-          <a-button
-            type="link"
-            style="color: red"
-            @click="onFilterItemDelete(index)"
-          >
-            删除
-          </a-button>
-        </div>
-      </template>
-    </a-table>
-    <div class="footer">
-      <div :class="queryArray.length ? 'show' : 'hide'" v-if="taskShow">
-        当前筛选条件下共包含
-        <span class="number">{{
-          typeof shwoTotal !== 'undefined' ? shwoTotal : total
-        }}</span>
-        {{
-          originalFilterData.find((i) => i.label === '群主')
-            ? '个客户群'
-            : '名用户'
-        }}
+        <a-button
+          v-if="!isEdit"
+          v-show="queryArray.length !== originalFilterData.length"
+          icon="plus"
+          @click="addFilter"
+          >添加筛选条件</a-button
+        >
+        <a-button icon="plus" @click="copy" v-if="taskShow && isEdit"
+          >以此条件再次创建任务
+        </a-button>
       </div>
-      <a-button
-        v-if="!isEdit"
-        v-show="queryArray.length !== originalFilterData.length"
-        icon="plus"
-        @click="addFilter"
-        >添加筛选条件</a-button
+      <a-modal
+        width="800px"
+        v-model="visable"
+        title="添加筛选条件"
+        :maskClosable="false"
+        @ok="onConfirm"
+        @cancel="onCancel"
       >
-      <a-button icon="plus" @click="copy" v-if="taskShow && isEdit"
-        >以此条件再次创建任务
-      </a-button>
-    </div>
-    <a-modal
-      width="800px"
-      v-model="visable"
-      title="添加筛选条件"
-      :maskClosable="false"
-      @ok="onConfirm"
-      @cancel="onCancel"
-    >
-      <div class="row">
-        <div class="label">筛选条件：</div>
-        <a-select
-          :value="modalData.currentKey"
-          style="width: 300px"
-          labelInValue
-          placeholder="请选择筛选条件"
-          showSearch
-          :filterOption="filterOption"
-          :options="filterData"
-          @change="onTypeChange"
-        ></a-select>
-      </div>
-      <div class="row">
-        <template v-if="currentSelectItem.type === 'input'">
-          <div class="label">筛选项目：</div>
-          <div>
-            <a-input
-              style="width: 300px"
-              allowClear
-              v-model="modalData.value"
-            ></a-input>
-          </div>
-        </template>
-        <template v-if="currentSelectItem.type === 'treeSelect'">
-          <div class="label">筛选项目：</div>
-          <a-tree-select
-            :value="modalData.value"
-            tree-default-expand-all
-            allow-clear
-            multiple
-            labelInValue
-            show-search
-            :filterTreeNode="
-              (input, options) =>
-                filterTreeNode(input, options, currentSelectItem)
-            "
-            style="width: 300px"
-            :dropdown-style="{ maxHeight: '300px', overflow: 'auto' }"
-            tree-checkable
-            placeholder="请选择渠道"
-            :replaceFields="
-              currentSelectItem.fieldNames || {
-                title: 'name',
-                key: 'id',
-                value: 'id',
-                children: 'children'
-              }
-            "
-            :tree-data="currentSelectOptions[currentSelectItem.value] || []"
-            @click="onDropdownVisibleChange(true, currentSelectItem)"
-            @change="(v) => onSelectValueChange(v)"
-          >
-          </a-tree-select>
-        </template>
-        <template v-if="currentSelectItem.type === 'select'">
-          <div class="label">筛选项目：</div>
+        <div class="row">
+          <div class="label">筛选条件：</div>
           <a-select
-            :value="
-              modalData.value
-                ? modalData.value.map(({ value, label }) => ({
-                    key: value,
-                    label
-                  }))
-                : []
-            "
+            :value="modalData.currentKey"
             style="width: 300px"
-            mode="multiple"
             labelInValue
-            :placeholder="`请选择${currentSelectItem.label}`"
+            placeholder="请选择筛选条件"
+            showSearch
             :filterOption="filterOption"
-            :options="currentSelectOptions[currentSelectItem.value] || []"
-            @dropdownVisibleChange="
-              (open) => onDropdownVisibleChange(open, currentSelectItem)
-            "
-            @change="
-              (v) =>
-                onSelectValueChange(
-                  v.map(({ key, label }) => ({
-                    value: key,
-                    label
-                  }))
-                )
-            "
+            :options="filterData"
+            @change="onTypeChange"
           ></a-select>
+        </div>
+        <div class="row">
+          <template v-if="currentSelectItem.type === 'input'">
+            <div class="label">筛选项目：</div>
+            <div>
+              <a-input
+                style="width: 300px"
+                allowClear
+                v-model="modalData.value"
+              ></a-input>
+            </div>
+          </template>
+          <template v-if="currentSelectItem.type === 'treeSelect'">
+            <div class="label">筛选项目：</div>
+            <a-tree-select
+              :value="modalData.value"
+              tree-default-expand-all
+              allow-clear
+              multiple
+              labelInValue
+              show-search
+              :filterTreeNode="
+                (input, options) =>
+                  filterTreeNode(input, options, currentSelectItem)
+              "
+              style="width: 300px"
+              :dropdown-style="{ maxHeight: '300px', overflow: 'auto' }"
+              tree-checkable
+              placeholder="请选择渠道"
+              :replaceFields="
+                currentSelectItem.fieldNames || {
+                  title: 'name',
+                  key: 'id',
+                  value: 'id',
+                  children: 'children'
+                }
+              "
+              :tree-data="currentSelectOptions[currentSelectItem.value] || []"
+              @click="onDropdownVisibleChange(true, currentSelectItem)"
+              @change="(v) => onSelectValueChange(v)"
+            >
+            </a-tree-select>
+          </template>
+          <template v-if="currentSelectItem.type === 'select'">
+            <div class="label">筛选项目：</div>
+            <a-select
+              :value="
+                modalData.value
+                  ? modalData.value.map(({ value, label }) => ({
+                      key: value,
+                      label
+                    }))
+                  : []
+              "
+              style="width: 300px"
+              mode="multiple"
+              labelInValue
+              :placeholder="`请选择${currentSelectItem.label}`"
+              :filterOption="filterOption"
+              :options="currentSelectOptions[currentSelectItem.value] || []"
+              @dropdownVisibleChange="
+                (open) => onDropdownVisibleChange(open, currentSelectItem)
+              "
+              @change="
+                (v) =>
+                  onSelectValueChange(
+                    v.map(({ key, label }) => ({
+                      value: key,
+                      label
+                    }))
+                  )
+              "
+            ></a-select>
+          </template>
+
+          <template v-if="currentSelectItem.type === 'cascader'">
+            <div class="label">筛选项目：</div>
+            <a-cascader
+              style="width: 300px"
+              placeholder="请选择"
+              :options="currentSelectOptions[currentSelectItem.value] || []"
+              :default-value="cascaderDefault"
+              change-on-select
+              :fieldNames="currentSelectItem.fieldNames"
+              @popupVisibleChange="
+                onDropdownVisibleChange($event, currentSelectItem)
+              "
+              @change="(v, options) => onCascaderValueChange(v, options)"
+            >
+            </a-cascader>
+          </template>
+
+          <template v-if="currentSelectItem.type === 'dateRange'">
+            <div class="label">筛选项目：</div>
+            <a-range-picker
+              :value="modalData.value"
+              format="YYYY-MM-DD"
+              :showTime="{
+                defaultValue: [moment().startOf('day'), moment().endOf('day')]
+              }"
+              :disabledDate="(current) => current > moment().endOf('day')"
+              @change="(v) => onDateRangeChange(v)"
+            ></a-range-picker>
+          </template>
+        </div>
+        <div
+          v-if="currentSelectItem.value === 'member_date' && modalData.value"
+          class="age_box"
+        >
+          {{ getAgeOfMonthStr(modalData.value[1]) }}
+          -
+          {{ getAgeOfMonthStr(modalData.value[0]) }}
+        </div>
+        <template v-if="currentSelectItem.type === 'tag'">
+          <Tag
+            :item="currentSelectItem"
+            :tagData="modalData"
+            @onChange="(data) => onSelectValueChange(data.value)"
+          ></Tag>
         </template>
 
-        <template v-if="currentSelectItem.type === 'cascader'">
+        <template v-if="currentSelectItem.type === 'age'">
           <div class="label">筛选项目：</div>
-          <a-cascader
-            style="width: 300px"
-            placeholder="请选择"
-            :options="currentSelectOptions[currentSelectItem.value] || []"
-            :default-value="cascaderDefault"
-            change-on-select
-            :fieldNames="currentSelectItem.fieldNames"
-            @popupVisibleChange="
-              onDropdownVisibleChange($event, currentSelectItem)
-            "
-            @change="(v, options) => onCascaderValueChange(v, options)"
-          >
-          </a-cascader>
+          <Ages
+            :item="currentSelectItem"
+            :ageData="modalData"
+            @onChange="getAgeVal"
+          ></Ages>
         </template>
-
-        <template v-if="currentSelectItem.type === 'dateRange'">
-          <div class="label">筛选项目：</div>
-          <a-range-picker
-            :value="modalData.value"
-            format="YYYY-MM-DD"
-            :showTime="{
-              defaultValue: [moment().startOf('day'), moment().endOf('day')]
-            }"
-            :disabledDate="(current) => current > moment().endOf('day')"
-            @change="(v) => onDateRangeChange(v)"
-          ></a-range-picker>
-        </template>
-      </div>
-      <div
-        v-if="currentSelectItem.value === 'member_date' && modalData.value"
-        class="age_box"
-      >
-        {{ getAgeOfMonthStr(modalData.value[1]) }}
-        -
-        {{ getAgeOfMonthStr(modalData.value[0]) }}
-      </div>
-      <template v-if="currentSelectItem.type === 'tag'">
-        <Tag
-          :item="currentSelectItem"
-          :tagData="modalData"
-          @onChange="(data) => onSelectValueChange(data.value)"
-        ></Tag>
-      </template>
-
-      <template v-if="currentSelectItem.type === 'age'">
-        <div class="label">筛选项目：</div>
-        <Ages
-          :item="currentSelectItem"
-          :ageData="modalData"
-          @onChange="getAgeVal"
-        ></Ages>
-      </template>
-    </a-modal>
+      </a-modal>
+    </div>
   </div>
 </template>
 
@@ -290,7 +292,7 @@ export default {
     },
     taskShow: {
       default() {
-        return true
+        return false
       }
     }
   },
@@ -609,38 +611,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.layout {
-  width: 800px;
-}
-.age_box {
-  margin-top: 10px;
-  margin-right: 34px;
-  text-align: right;
-}
-.footer {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 20px;
-}
-.number {
-  color: #1785ff;
-}
-.show {
-  opacity: 1;
-}
-.hide {
-  opacity: 0;
-}
-.row {
-  display: flex;
-  align-items: center;
-  .label {
-    margin-right: 20px;
-    width: 70px;
+#content {
+  .layout {
+    width: 800px;
   }
-}
-.row + .row {
-  margin-top: 20px;
+  .age_box {
+    margin-top: 10px;
+    margin-right: 34px;
+    text-align: right;
+  }
+  .footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+  .number {
+    color: #1785ff;
+  }
+  .show {
+    opacity: 1;
+  }
+  .hide {
+    opacity: 0;
+  }
+  .row {
+    display: flex;
+    align-items: center;
+    .label {
+      margin-right: 20px;
+      width: 70px;
+    }
+  }
+  .row + .row {
+    margin-top: 20px;
+  }
 }
 </style>
